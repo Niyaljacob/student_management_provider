3af3830068a718a659c02c381790de14fa136832
niyal jacob <niyaljacob76@gmail.com>
2024-05-28 11:34:21 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:student_manag_provider/provider/splash_screen_provider.dart';
+import 'package:student_manag_provider/view/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=> SplashProvider()),
+      ],
+      child: MaterialApp(
+        
+        debugShowCheckedModeBanner: false,
+        title: 'Students',
+        theme: ThemeData(
+         primarySwatch: Colors.blue
+         
+         
+        ),
+        home:SplashScreen(),
+      
+      ),
+    );
+  }
+}
